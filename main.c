c50aedc75b0924b247103ccba2cf43522c28de0c
rzboxx-expPs2 <rzboxx@gmail.com>
2026-01-20 19:02:24 +0700

    add ps2 files

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <tamtypes.h>
+#include <kernel.h>
+#include <sifrpc.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Dragon Ball Ultimate Saiyan X RZ loaded!\n");
+    SleepThread();
+    return 0;
+}
